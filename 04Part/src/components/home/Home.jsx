563c37da240979b631eca67db46382a687aa521c
PujaPKS <pkpuja2404@gmail.com>
2024-12-04 01:05:04 +0530

    Implemented About page

---
+++ 04Part/src/components/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Home.css';
+
+const Home = () => {
+  return (
+    <>
+      <h1 className="top mt-4 pt-5 pb-5">The Generics</h1>
+      <h1 className="home">Welcome to Home Page</h1>
+    </>
+  );
+};
+
+export default Home;
